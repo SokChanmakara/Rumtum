b7c9275ef01b63601976a19e23bf4d083a34944b
chhay <soeungkimchhay50@gmail.com>
2024-11-29 16:15:51 +0700

    it is my work ,i known right or worng .pls fix .

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<script>
+import { RouterLink } from 'vue-router';
+export default {
+    name: "Navbar",
+    props : {
+        Nav_name : String,
+        Link : String,
+        Image : String,
+        isSelectRoute : Boolean
+    }
+}
+</script>
+<template>
+    <nav>
+        <ul>
+            <li>
+                <RouterLink :class="{active : isSelectRoute}"  class="link" :to=Link>
+                    <img :src="Image" alt="">
+                    {{ Nav_name }}
+                </RouterLink>
+            </li>
+        </ul>
+    </nav>
+
+</template>
