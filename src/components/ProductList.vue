7e0b6acd8aa5dd950dbe57ebcebdaa5d2d98f6d2
SokChanmakara <sokchanmakara04@gmail.com>
2024-12-18 19:43:45 +0700

    Add hover effect for productlist to show wishlist, quick view and quick shop

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,6 +1,28 @@
 <template>
     <div class="container">
-        <img :src="isHovered ? Himage : images" alt="" @mouseenter="isHovered = true" @mouseleave="isHovered = false">
+        <div class="icon-wrapper" :style="{ opacity: isHovered ? 1 : 0 }" 
+            @mouseenter="isHovered = true" 
+            @mouseleave="isHovered = false">
+            <div class="icon-text">
+                <div class="icon">
+                    <i class="pi pi-heart"></i>
+                </div>
+                <p>Add to Wishlist</p>
+            </div>
+            <div class="icon-text">
+                <div class="icon">
+                    <i class="pi pi-shopping-cart"></i>
+                </div>
+                    <p>Quick Shop</p>
+            </div>
+            <div class="icon-text">
+                <div class="icon">
+                    <i class="pi pi-search-plus"></i>
+                </div>
+                <p>Quick View</p>
+            </div>
+        </div>
+        <img :src="isHovered ? Himage : images" :alt="name" @mouseenter="isHovered = true" @mouseleave="isHovered = false">
         <div class="color-picker">
             <div class="color one" :style="{ backgroundColor: pri_color }"></div>
             <div class="color two" :style="{backgroundColor: sec_color}"></div>
@@ -36,8 +58,51 @@ export default {
 
 <style scoped>
     .container{
+        position:relative;
         /* margin-left: 23px; */
         margin-top: 23px;
+    }
+    .icon-wrapper{
+        position:absolute;
+        display:flex;
+        gap:10px;
+        flex-direction: column;
+        top:55%;
+        left:15px;
+    }
+    .icon-text{
+        display:flex;
+    }
+    .icon-text > p{
+        display:none;
+        font-size: 16px;
+        color:white;
+        background: black;
+        padding:8px;
+        margin-left:3px;
+        border-radius: 40px;
+        border-bottom-left-radius: 100px ;
+        border-top-left-radius: 100px;
+    }
+    .icon-text:hover > p{
+        display:block;
+        transition: 0.3s ease;
+    }
+    .icon{
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        width:40px;
+        height:40px;
+        border-radius: 50%;
+        padding:8px;
+        background: white;
+    }
+
+    .icon:hover{
+        color:white;
+        background: black;
+        transition: 0.3s ease;
     }
     img{
         width:340px;
